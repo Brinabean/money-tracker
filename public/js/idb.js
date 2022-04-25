68b6591e5153d74779a320e8de48752385ede548
Brina <sabrinazanin97@gmail.com>
2022-04-24 21:48:55 -0400

    indexdb and started service-worker

---
+++ public/js/idb.js
@@ -0,0 +1,38 @@
+// create variable to hold db connection
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function (event) {
+  // save a reference to the database
+  const db = event.target.result;
+
+  db.createObjectStore("new_transaction", { autoIncrement: true });
+};
+
+// upon a successful
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    // we haven't created this yet, but we will soon, so let's comment it out for now
+    // uploadPizza();
+  }
+};
+
+request.onerror = function (event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  const transactionObjectStore = transaction.objectStore("new_transaction");
+
+  // add record to your store with add method
+  transactionObjectStore.add(record);
+}
